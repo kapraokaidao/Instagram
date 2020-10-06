c3a1330a270c896619f2fe6c9367c125e31b5d21
N <n@n.n>
2020-10-06 14:39:58 +0700

    update profile

--- frontend/instagram/src/App.vue
+++ frontend/instagram/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app>
     <v-main class="bg">
-      <router-view />
+      <v-container>
+        <router-view />
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -22,10 +24,81 @@ export default class App extends Vue {
 }
 </script>
 
-<style lang="scss">
+<style>
 .bg {
-  background-image: url("assets/cat-background.jpg");
+  /*background-image: url("assets/blur-cat-background.jpg");
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size: cover;*/
+  background-color: white;
+}
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap");
+.flex-container {
+  height: 100%;
+  margin-inline-start: 10%;
+  margin-inline-end: 10%;
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+}
+.card-style-login {
+  width: 35em;
+  padding: 30px;
+  background: #ffffff;
+  border-radius: 32px;
+}
+.form-style-login {
+  text-align: center;
+  margin: 5em 3em;
+  display: flex;
+  flex-direction: column;
+}
+.img-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.header {
+  text-align: center;
+
+  font-family: "Quicksand";
+  font-weight: bold;
+  font-size: 36px;
+}
+.login-btn {
+  font-family: "Quicksand";
+  font-weight: bold;
+  font-size: 18px;
+}
+.signup-link {
+  margin: 1em;
+}
+.signup-text {
+  font-family: "Quicksand";
+  font-weight: bold;
+}
+.text-field {
+  font-family: "Quicksand";
+  font-weight: regular;
+}
+.card-style-signup {
+  margin: 0 auto;
+  width: 30%;
+  background: #ffffff;
+  padding: 20px;
+  margin-top: 10%;
+}
+.form-style-signup {
+  width: 70%;
+  margin: 0 auto;
+  padding: 10px;
+}
+.register-btn {
+  margin: auto;
+  width: 70%;
+}
+.header {
+  text-align: center;
+  margin-top: 10px;
+  display: block;
 }
 </style>
--- frontend/instagram/src/views/UpdateProfile.vue
+++ frontend/instagram/src/views/UpdateProfile.vue
@@ -1,15 +1,86 @@
 <template>
   <div>
-    vue update
+    <h1 className="head">Edit Profile</h1>
+    <v-row no-gutters>
+      <v-col>
+        <v-card class="ma-4 pa-4">
+          <v-text-field
+            v-model="username"
+            :rules="[rules.required]"
+            label="Username"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password1"
+            :append-icon="showPassword1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="showPassword1 ? 'text' : 'password'"
+            label="Password"
+            hint="At least 8 characters"
+            counter
+            @click:append="showPassword1 = !showPassword1"
+          ></v-text-field>
+          <v-text-field
+            v-model="password2"
+            :append-icon="showPassword2 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="showPassword2 ? 'text' : 'password'"
+            label="Password"
+            hint="At least 8 characters"
+            counter
+            @click:append="showPassword2 = !showPassword2"
+          ></v-text-field>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-file-input
+          label="Upload new picture"
+          filled
+          prepend-icon="mdi-camera"
+        ></v-file-input>
+        <v-textarea solo name="bio" label="Bio"></v-textarea>
+      </v-col>
+    </v-row>
+    <v-btn
+      class="register-btn my-2"
+      @click="submit"
+      color="#727272"
+      to="/"
+      large
+      dark
+    >
+      Submit Edit
+    </v-btn>
   </div>
 </template>
 
 <script>
 export default {
   name: "UpdateProfile",
-  data: () => ({}),
+  data: () => ({
+    username: "",
+    showPassword1: false,
+    showPassword2: false,
+    password1: "",
+    password2: "",
+    rules: {
+      required: (value) => !!value || "Required.",
+      min: (v) => v.length >= 8 || "Min 8 characters",
+      emailMatch: () => "The email and password you entered don't match",
+    },
+  }),
   methods: {},
 };
 </script>
 
-<style scope lang="scss"></style>
+<style scoped>
+.head {
+  font-family: Quicksand;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 45px;
+
+  color: #515151;
+}
+</style>
