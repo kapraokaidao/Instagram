b744af82bf2afe13d72bab1772e1e7a7abef4c0d
Kao Oat <tanawit139@gmail.com>
2020-10-17 21:22:11 +0700

    backend: integrate serverless

--- backend/instagram/src/main.ts
+++ backend/instagram/src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory, Reflector } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { JwtAuthGuard } from "./auth/jwt-auth.guard";
+import { Express } from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,4 +21,24 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
-bootstrap();
+
+export async function createApp(
+  expressApp: Express,
+): Promise<INestApplication> {
+
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  app.enableCors();
+  app.useGlobalGuards(new JwtAuthGuard(app.get(Reflector)));
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle("Instagram")
+    .setVersion("0.1")
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api", app, document);
+
+  return app;
+}
+
+// bootstrap();
---
+++ backend/instagram/src/serverless.ts
@@ -0,0 +1,24 @@
+import { Server } from 'http';
+import { Context } from 'aws-lambda';
+import { createServer, proxy, Response } from 'aws-serverless-express';
+import * as express from 'express';
+import { createApp } from './main';
+
+let cachedServer: Server;
+
+async function bootstrap(): Promise<Server> {
+  const expressApp = express();
+
+  const app = await createApp(expressApp);
+  await app.init();
+
+  return createServer(expressApp);
+}
+
+export async function handler(event: any, context: Context): Promise<Response> {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+}
